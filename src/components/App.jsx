f76b40d917b4ade3e3d7c90258d064a055919173
abhijit <abhidutta338@gmail.com>
2021-05-17 14:14:42 +0530

    made todo list

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function App() {
+  const [items, setitems] = React.useState([]);
+  const [mean, setmean] = React.useState("");
+
+  function onChange(event) {
+    var value = event.target.value;
+    setmean(value);
+  }
+
+  function onClick() {
+    setitems((prevValue) => {
+      return [...prevValue, mean];
+    });
+    setmean("");
+  }
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={onChange} type="text" value={mean} />
+        <button onClick={onClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item) => {
+            return <li>{item}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
